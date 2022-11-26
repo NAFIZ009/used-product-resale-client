9dd1d64b5663bcbe42280786b175059f5d73c5c6
NAFIZ009 <jalalahmednafiz@gmail.com>
2022-11-26 23:39:56 +0600

    sec

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,10 +1,10 @@
 import React from 'react';
-
+import styles from './home.module.css';
 const Home = () => {
     return (
         <div>
-            <section className='banner-sec min-h-[430px] max-w-screen-lg mx-auto bg-white'>
-
+            <section className={`${styles.bannerSec} relative min-h-[430px] max-w-screen-lg mx-auto bg-white`}>
+                <span className='absolute top-0 left-2/4'>ANDROID TV</span>
             </section>
         </div>
     );
